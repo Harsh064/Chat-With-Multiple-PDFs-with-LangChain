a394b4cfe727b990632a1563a444e4b778ecb739
Harsh <harshnesj@gmail.com>
2024-12-28 02:14:13 +0530

    commit

---
+++ htmlTemplates.py
@@ -0,0 +1,44 @@
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://media.istockphoto.com/id/1349170472/photo/artificial-intelligence-robot-or-cyborg-portrait.jpg?s=612x612&w=0&k=20&c=y2IUXDHGn1vzZrVT3Vg9bMCVO4tZziLTToBzblxqza8=" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://easy-peasy.ai/cdn-cgi/image/quality=80,format=auto,width=700/https://fdczvxmwwjwpwbeeqcth.supabase.co/storage/v1/object/public/images/3ef25463-241c-425b-93b2-816065b61ff7/9d7291de-f849-4071-917b-ff200b756194.png">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
